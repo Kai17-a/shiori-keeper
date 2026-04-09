b20e19eed728d36b330d604744eee20dab9cb708
Kai17-a <k.s25skt@gmail.com>
2026-04-09 22:20:50 +0900

    refactor(frontend): remove redundant API mirror

--- frontend/app/pages/api/conftest.py
+++
@@ -1,7 +0,0 @@
-from api.tests.test_support import CompatTestClient
-
-
-def pytest_configure():
-    import fastapi.testclient as fastapi_testclient
-
-    fastapi_testclient.TestClient = CompatTestClient
